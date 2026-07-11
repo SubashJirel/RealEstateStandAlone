bbd93e63eb9fc4b004ae58cab63ce28e3231e48a
Subash Jirel <subashjrl@gmail.com>
2026-07-11 20:32:52 +0545

    refactor: comment out unused components in SiteChrome and clean up HomePage

--- app/components/layout/SiteChrome.tsx
+++ app/components/layout/SiteChrome.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { Footer } from "@/components/layout/Footer";
-import { Navbar } from "@/components/layout/Navbar";
+// import { Footer } from "@/components/layout/Footer";
+// import { Navbar } from "@/components/layout/Navbar";
 
 export function SiteChrome({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
@@ -14,9 +14,9 @@ export function SiteChrome({ children }: { children: React.ReactNode }) {
 
   return (
     <>
-      <Navbar />
+      {/*<Navbar />*/}
       <main>{children}</main>
-      <Footer />
+      {/*<Footer />*/}
     </>
   );
 }
